48d6a74edbb7667a17ca550be5741816156b3848
MinysJava <poforli@gmail.com>
2019-09-29 18:03:26 +0500

    Урок №2 Первая часть

---
+++ webapp/src/main/java/ru/gb/servlet/FirstServlet.java
@@ -0,0 +1,36 @@
+package ru.gb.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class FirstServlet implements Servlet, Serializable {
+
+    private transient ServletConfig config;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.config = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("<h1>Hello from FirstServlet</h1>");
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "FirstServlet";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
